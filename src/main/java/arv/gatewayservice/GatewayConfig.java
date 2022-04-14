55eb858bf241b6834e1fcdc5d29db70c762ed358
ArvikVan <arvikvan@gmail.com>
2022-04-14 15:28:22 +0300

    yml_created

--- src/main/java/arv/gatewayservice/GatewayConfig.java
+++ src/main/java/arv/gatewayservice/GatewayConfig.java
@@ -1,8 +1,10 @@
 package arv.gatewayservice;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,15 +13,14 @@ import org.springframework.context.annotation.Configuration;
  * @version 1.0
  * @since 13.04.2022
  */
-@Configuration
+//@Configuration
 public class GatewayConfig {
-    @Bean
-    @LoadBalanced
+  /*  @Bean
     public RouteLocator getPathsToServices(RouteLocatorBuilder builder) {
         return builder.routes()
                 //.route(r -> r.path("/**").uri("http://localhost:8083"))
-                .route(r -> r.path("/user-service/**").uri("http://localhost:8081"))
-                .route(r -> r.path("/**").uri("http://localhost:8085"))
+                .route(r -> r.path("/first-service/**").uri("http://localhost:8085"))
+                .route(r -> r.path("/**").uri("http://localhost:8081"))
                 .build();
-    }
+    }*/
 }
